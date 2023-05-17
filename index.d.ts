e856b113c902b60fb725e14c95f75a606363372b
allisonrod <117668984+allisonrod@users.noreply.github.com>
2023-05-16 19:06:16 -0500

    fix: add missing schema grouping names to SchemaGroupings (#17)

--- index.d.ts
+++ index.d.ts
@@ -339,6 +339,9 @@ export interface CredentialSchemaData {
 export interface SchemaGroupings {
   name?: CredentialSchemaData | undefined;
   contactInfo?: CredentialSchemaData | undefined;
+  miscellaneous?: CredentialSchemaData | undefined;
+  document?: CredentialSchemaData | undefined;
+  [key: string]: CredentialSchemaData | undefined;
 }
 
 export interface SchemaPresentationDto {
