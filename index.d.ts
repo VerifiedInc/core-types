5984dcd8dba6f49ce4a3e95642d50f653ef5bd73
Iagor Moraes <iagor2014@outlook.com>
2023-08-10 17:40:14 -0300

    fix: remove enum syntax

--- index.d.ts
+++ index.d.ts
@@ -97,9 +97,9 @@ export interface AuthenticationResult {
  * Type of possible options for mandatory field a credential request.
  */
 export const MandatoryEnum = {
-  YES = 'yes',
-  NO = 'no',
-  IF_AVAILABLE = 'if_available'
+  YES: 'yes',
+  NO: 'no',
+  IF_AVAILABLE: 'if_available'
 } as const
 
 /**
