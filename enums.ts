8e37f1269f021b2a21d95c80ed87bb7142d6c043
Iagor Moraes <iagor2014@outlook.com>
2023-08-18 14:06:09 -0300

    chore: add display format enum

--- enums.ts
+++ enums.ts
@@ -6,3 +6,16 @@ export enum MandatoryEnum {
   NO = 'no',
   IF_AVAILABLE = 'if_available'
 }
+
+export enum DisplayFormatEnum {
+  Image = 'Image',
+  Phone = 'Phone',
+  Ssn = 'SSN',
+  Date = 'Date',
+  Address = 'Address',
+  CurrencyRange = 'CurrencyRange',
+  String = 'String',
+  State = 'State',
+  Number = 'Number',
+  CurrencyAmount = 'CurrencyAmount',
+}
