9e3161df6b9592a63e2de455a6992010c319e5f3
Iagor Moraes <iagor2014@outlook.com>
2024-01-29 16:56:00 -0300

    chore: add semi-colon

--- src/types.ts
+++ src/types.ts
@@ -179,7 +179,7 @@ export interface PresentationRequestDto {
   description?: string | null;
   title?: string | null;
   redirectUrl?: string | null;
-  isOneClick: boolean
+  isOneClick: boolean;
 }
 
 /**
